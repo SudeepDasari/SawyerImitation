f306ac7c7f619261b8b6e74aa230bd1c06dfa4d4
Annie Xie <anniexie@airbears2-10-142-76-253.airbears2.1918.berkeley.edu>
2017-09-19 10:41:09 -0700

    process images and pkls

--- read_tf_records.py
+++ read_tf_records.py
@@ -3,6 +3,8 @@ import numpy as np
 from tensorflow.python.platform import gfile
 import cv2
 import os
+import cPickle
+import glob
 
 
 def _float_feature(value):
@@ -17,7 +19,7 @@ def _int64_feature(value):
   return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
 
-def write_tf_records(images, actions, states, filepath):
+def write_tf_records(images, angles, velocities, states, filepath):
     filename = os.path.join(dir, filepath + '/tfrecords/demo_examples.tfrecords')
     print('Writing', filename)
     writer = tf.python_io.TFRecordWriter(filename)
@@ -25,17 +27,36 @@ def write_tf_records(images, actions, states, filepath):
     feature = {}
 
     for ex in range(images.shape[0]):
-        sequence_length = 60
+        image_raw = (images[ex]*255.).astype(np.uint8)
+        image_raw = image_raw.tostring()
 
-        for tind in range(sequence_length):
-            image_raw = (images[ex,tind]*255.).astype(np.uint8)
-            image_raw = image_raw.tostring()
-
-            feature[str(tind) + '/action'] = _float_feature(actions[ex, tind].tolist())
-            feature[str(tind) + '/endeffector_pos'] = _float_feature(states[ex,tind].tolist())
-            feature[str(tind) + '/image'] = _bytes_feature(image_raw)
+        feature['angle'] = _float_feature(angles[ex].tolist())
+        feature['velocity'] = _float_feature(velocities[ex].tolist())
+        feature['endeffector_pos'] = _float_feature(states[ex].tolist())
+        feature['image'] = _bytes_feature(image_raw)
 
         example = tf.train.Example(features=tf.train.Features(feature=feature))
         writer.write(example.SerializeToString())
 
     writer.close()
+
+
+def main():
+    images, angles, velocities, states = [], [], [], []
+
+    # for i in range(100):
+    #     for filename in glob.glob('traj{}/images/*.png'.format(i)):
+    #         images += []
+    #
+    #     with open('joint_traj{}.pkl'.format(i), 'r') as f:
+    #         data = cPickle.load(f)
+    #     angles += data['jointangles']
+    #     velocities += data['jointvelocities']
+    #     states += data['endeffector_pos']
+    #
+    # filepath = ''
+    # write_tf_records(images, angles, velocities, states, filepath)
+
+
+if __name__ == '__main__':
+    main()
