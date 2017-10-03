f7a304109f75d04eece18d3d43d77f528c890a09
Annie Xie <anniexie@airbears2-10-142-38-105.airbears2.1918.berkeley.edu>
2017-10-03 11:06:35 -0700

    Pasted recorder, controller, deploy code

--- imitation_rospkg/src/deploy_imitation.py
+++ imitation_rospkg/src/deploy_imitation.py
@@ -1,2 +1,141 @@
 from train_net import Model
 import tensorflow as tf
+import numpy as np
+import argparse
+import rospy
+from sensor_msgs.msg import Image as Image_msg
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+from intera_interface import CHECK_VERSION
+import intera_interface
+from sensor_msgs.msg import JointState
+from intera_core_msgs.srv import (
+    SolvePositionFK,
+    SolvePositionFKRequest,
+)
+
+from robot_controller import RobotController
+from recorder.robot_recorder import RobotRecorder
+
+class SawyerImitation(object):
+    def __init__(self):
+
+        parser = argparse.ArgumentParser()
+        self.args = parser.parse_args()
+        parser.add_argument('--save_dir', default='./', type=str, help='')
+
+        print("Initializing node... ")
+        rospy.init_node("sawyer_imitation_client")
+
+        self.rs = intera_interface.RobotEnable(CHECK_VERSION)
+        self.init_state = self.rs.state().enabled
+
+        self.ctrl = RobotController()
+
+        self.limb = intera_interface.Limb("right")
+        self.joint_names = self.limb.joint_names()
+
+        self.recorder = RobotRecorder(save_dir=self.args.save_dir,
+                                      seq_len=60,
+                                      use_aux=self.use_aux,
+                                      save_video=True,
+                                      save_actions=False,
+                                      save_images=False)
+
+        self.name_of_service = "ExternalTools/right/PositionKinematicsNode/FKService"
+        self.fksvc = rospy.ServiceProxy(self.name_of_service, SolvePositionFK)
+
+        self.bridge = CvBridge()
+
+        self.action_interval = 1 #Hz
+        self.action_sequence_length = 60
+        self.traj_duration = self.action_sequence_length * self.action_interval
+        self.action_rate = rospy.Rate(self.action_interval)
+        self.control_rate = rospy.Rate(20)
+
+        self.get_action_func = rospy.ServiceProxy('get_action', get_action)
+
+        # self.imp_ctrl_publisher = rospy.Publisher('desired_joint_pos', JointState, queue_size=1)
+        # self.imp_ctrl_release_spring_pub = rospy.Publisher('release_spring', Float32, queue_size=10)
+        # self.imp_ctrl_active = rospy.Publisher('imp_ctrl_active', Int64, queue_size=10)
+        # self.imp_ctrl_active.publish(0)
+
+        rospy.on_shutdown(self.ctrl.clean_shutdown)
+
+        self.ctrl.set_neutral()
+
+    # def start_recording(self, value):
+    #     if not value:
+    #         return
+    #
+    #     if self.collect_active:
+    #         return
+    #     self.recorder.init_traj(self.record_iter)
+    #     self.record_iter += 1
+    #
+    #     self.collect_active = True
+    #     self.imp_ctrl_active.publish(0)
+    #     # self.joint_pos = []
+    #
+    #     iter = 0
+    #     while (self.collect_active):
+    #         self.control_rate.sleep()
+    #         # self.joint_pos.append(self.limb.joint_angles())
+    #         pose = self.get_endeffector_pos()
+    #         print 'recording ', iter
+    #         self.recorder.save(iter, pose)
+    #         iter += 1
+    #
+    #         if (iter >= self.N_SAMPLES):
+    #             self.collect_active = False
+    #
+    #             # filename = '/home/sudeep/outputs/pushback_traj_.pkl'
+    #             # with open(filename, 'wb') as f:
+    #             #     cPickle.dump(self.joint_pos, f)
+    #
+    #             # print 'saved file to ', filename
+
+    def query_action(self):
+
+        if self.use_aux:
+            self.recorder.get_aux_img()
+            imageaux1 = self.recorder.ltob_aux1.img_msg
+        else:
+            imageaux1 = np.zeros((64, 64, 3), dtype=np.uint8)
+            imageaux1 = self.bridge.cv2_to_imgmsg(imageaux1)
+
+        imagemain = self.bridge.cv2_to_imgmsg(self.recorder.ltob.img_cropped)
+        robot_configs = self.get_endeffector_pos() # need to add joint angles here
+
+        try:
+            rospy.wait_for_service('get_action', timeout=240)
+            get_action_resp = self.get_action_func(imagemain, tuple(robot_configs))
+
+            action_vec = get_action_resp.action
+
+        except (rospy.ServiceException, rospy.ROSException), e:
+            rospy.logerr("Service call failed: %s" % (e,))
+            raise ValueError('get action service call failed')
+        return action_vec
+
+    def get_endeffector_pos(self):
+        fkreq = SolvePositionFKRequest()
+        joints = JointState()
+        joints.name = self.limb.joint_names()
+        joints.position = [self.limb.joint_angle(j)
+                           for j in joints.name]
+
+        # Add desired pose for forward kinematics
+        fkreq.configuration.append(joints)
+        fkreq.tip_names.append('right_hand')
+        try:
+            rospy.wait_for_service(self.name_of_service, 5)
+            resp = self.fksvc(fkreq)
+        except (rospy.ServiceException, rospy.ROSException), e:
+            rospy.logerr("Service call failed: %s" % (e,))
+            return False
+
+        pos = np.array([resp.pose_stamp[0].pose.position.x,
+                        resp.pose_stamp[0].pose.position.y,
+                        resp.pose_stamp[0].pose.position.z])
+        return pos
---
+++ imitation_rospkg/src/robot_controller.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import argparse
+import rospy
+
+import socket
+import intera_interface
+import intera_external_devices
+from intera_interface import CHECK_VERSION
+
+import numpy as np
+import socket
+import pdb
+
+
+class RobotController(object):
+
+    def __init__(self):
+        """Initializes a controller for the robot"""
+
+        print("Initializing node... ")
+        rospy.init_node("sawyer_custom_controller")
+        rospy.on_shutdown(self.clean_shutdown)
+
+        rs = intera_interface.RobotEnable(CHECK_VERSION)
+        init_state = rs.state().enabled
+        print("Robot enabled...")
+
+        self.limb = intera_interface.Limb("right")
+
+        self.joint_names = self.limb.joint_names()
+        print("Done initializing controller.")
+
+    def set_joint_delta(self, joint_name, delta):
+        """Move a single joint by a delta"""
+        current_position = self.limb.joint_angle(joint_name)
+        self.set_joint(joint_name, current_position + delta)
+
+    def set_joint_position(self, joint_name, pos):
+        """Move a single joint to a target position"""
+        joint_command = {joint_name: pos}
+        self.limb.set_joint_positions(joint_command)
+
+    def set_joint_positions(self, positions):
+        """Move joints to commmand"""
+        self.limb.move_to_joint_positions(positions)
+
+    def set_joint_velocities(self, velocities):
+        self.limb.set_joint_velocities(velocities)
+
+    def set_neutral(self, speed=.2):
+        # using a custom handpicked neutral position
+        # starting from j0 to j6:
+        neutral_jointangles = [0.412271, -0.434908, -1.198768, 1.795462, 1.160788, 1.107675, 2.068076]
+        cmd = dict(zip(self.joint_names, neutral_jointangles))
+
+        self.limb.set_joint_position_speed(speed)
+
+        done = False
+        while not done:
+            try:
+                self.set_joints(cmd)
+            except:
+                print 'retrying set neutral...'
+
+            done = True
+
+    def clean_shutdown(self):
+        print("\nExiting example.")
+        # if not init_state:
+        #     print("Disabling robot...")
+            # rs.disable()
