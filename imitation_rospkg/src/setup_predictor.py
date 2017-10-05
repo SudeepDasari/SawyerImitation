6ee2802917bda2be6436d3e5d2fd46bd5d1ddb4d
Sudeep Dasari <sdasari@berkeley.edu>
2017-10-05 10:19:17 -0700

    updated copy of setup predictor

--- imitation_rospkg/src/setup_predictor.py
+++ imitation_rospkg/src/setup_predictor.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
-import imp
 import numpy as np
 from imitation_learning import ImitationLearningModel
 import os
 import cv2
 import time
 
+
 def setup_predictor(model_path, vgg19_path):
-    images_pl = tf.placeholder(tf.float32, name='images', shape=(1, 64, 64, 3))
+    images_pl = tf.placeholder(tf.uint8, name='images', shape=(1, 224, 224, 3))
     configs_pl = tf.placeholder(tf.float32, name='configs', shape=(1, 10))
 
     with tf.variable_scope('model', reuse=None):
         model = ImitationLearningModel(vgg19_path, images_pl, configs_pl)
         model.build()
 
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
 
     vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
     saver = tf.train.Saver(vars, max_to_keep=0)
@@ -27,7 +27,7 @@ def setup_predictor(model_path, vgg19_path):
     saver.restore(sess, model_path)
 
     def predictor_func(images=None, robot_configs=None):
-        feed_image = images.astype(np.float32).reshape((1, 64, 64, 3))
+        feed_image = images.astype(np.uint8).reshape((1, 224, 224, 3))
         feed_config = robot_configs.astype(np.float32).reshape((1, 10))
 
         feed_dict = {
